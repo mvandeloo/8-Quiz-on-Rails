f6d961d00fef157989ddb4ffd9c0476369434e18
lorraine-phill <lp3767-web@yahoo.com>
2013-10-08 17:38:26 +0100

    day 1 passed

--- quiz_app/db/schema.rb
+++ quiz_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131008112247) do
+ActiveRecord::Schema.define(version: 20131008162918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20131008112247) do
   create_table "quizzes", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "title"
   end
 
 end
--- quiz_app/spec/features/quiz_feature_spec.rb
+++ quiz_app/spec/features/quiz_feature_spec.rb
@@ -1,9 +1,22 @@
 require 'spec_helper'
 
+	def create_quiz(sometitle)
+		Quiz.create(:title => sometitle)
+	end
+
+
+	describe 'the quizzes section' do
+
+	 
+	before(:all) do
+    	create_quiz 'Some quiz'
+  	end
+
 	describe '/quizzes' do
 		it 'should display quizzes' do
-		visit '/quizzes'
-
-		expect(page).to have_content 'Some quiz'	
-	end	
+			visit '/quizzes'
+			expect(page).to have_content 'Some quiz'	
+		end	
 	end
+
+end
